4160a3f2d153e6e0f48494ae411c248b3e286b22
MarcelTheMarcel <marcel.durza@gmail.com>
2024-07-25 09:34:42 +0200

    lekcia 25

---
+++ java-basics/src/main/java/academy/metis/javabasics/lesson25/activity3/exercise1/part1/Book.java
@@ -0,0 +1,75 @@
+package academy.metis.javabasics.lesson25.activity3.exercise1.part1;
+
+public class Book {
+    private String title;
+    private String authorName;
+    private String isbn;
+    private final int pageCount;
+    private int availableCopies;
+    private final long titleId;
+    private final String titleType;
+
+    public Book(String title, String authorName, String isbn, int pageCount, int availableCopies, long titleId, String titleType) {
+        this.title = title;
+        this.authorName = authorName;
+        this.isbn = isbn;
+        this.pageCount = pageCount;
+        this.availableCopies = availableCopies;
+        this.titleId = titleId;
+        this.titleType = titleType;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    public String getTitleType() {
+        return titleType;
+    }
+
+    public long getTitleId() {
+        return titleId;
+    }
+
+    @Override
+    public String toString() {
+        return titleId +
+                " | Name: " + title +
+                " | Author: " + authorName +
+                " | ISBN: " + isbn +
+                " | Number of pages: " + pageCount +
+                " | Available copies: " + availableCopies;
+    }
+}
---
+++ java-basics/src/main/java/academy/metis/javabasics/lesson25/activity3/exercise1/part1/QueueRecord.java
@@ -0,0 +1,52 @@
+package academy.metis.javabasics.lesson25.activity3.exercise1.part1;
+
+import java.time.LocalDate;
+
+public class QueueRecord {
+    private final long queueItemId;
+    private final long memberId;
+    private final long titleId;
+    private final LocalDate addedToQueueDate;
+    private boolean isResolved;
+
+    public QueueRecord(long queueItemId, long memberId, long titleId, LocalDate addedToQueueDate, boolean isResolved) {
+        this.queueItemId = queueItemId;
+        this.memberId = memberId;
+        this.titleId = titleId;
+        this.addedToQueueDate = addedToQueueDate;
+        this.isResolved = isResolved;
+    }
+
+    public long getQueueItemId() {
+        return queueItemId;
+    }
+
+    public long getMemberId() {
+        return memberId;
+    }
+
+    public long getTitleId() {
+        return titleId;
+    }
+
+    public LocalDate getAddedToQueueDate() {
+        return addedToQueueDate;
+    }
+
+    public boolean isResolved() {
+        return isResolved;
+    }
+
+    public void setResolved(boolean resolved) {
+        isResolved = resolved;
+    }
+
+    @Override
+    public String toString() {
+        return queueItemId +
+                ". | Member Id = " + memberId +
+                "| Title Id = " + titleId +
+                "| Added to Queue = " + addedToQueueDate +
+                "| Resolved = " + ((isResolved) ? "RESOLVED" : "NOT RESOLVED");
+    }
+}
